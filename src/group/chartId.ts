4ace079a79fb427ebf6e7ef82cf4f2bad5aa17c5
neoddish <neoddish@outlook.com>
2022-04-11 23:28:26 +0800

    refactor: todo group chartId

--- src/group/chartId.ts
+++ src/group/chartId.ts
@@ -1,8 +1,9 @@
-import * as dataSamples from '@src/data';
+import * as ds from '@src/data';
 
 import type { ChartID } from '@antv/ckb';
 import type { DataSample } from '@src/data/interface';
 
 export const DATA_FOR_CHART_TYPE: Partial<Record<ChartID, DataSample>> = {
-  bar_chart: dataSamples.cars,
+  // TODO fill all chart types
+  bar_chart: ds.barChartTestData,
 };
