519ca6ec710943656cec4ad6eff333e3eee95c31
GuangMingYouBei <hz2149@nyu.edu>
2022-04-18 11:27:06 +0800

    fix: update validated test data for chart-advisor

---
+++ src/data/test-data/column_chart-test/index.ts
@@ -0,0 +1,10 @@
+import * as data from './column_chart-test-data.json';
+
+import type { DataSample } from '../../interface';
+
+const dataSample: DataSample = {
+  name: 'column_chart-test-data',
+  data,
+};
+
+export default dataSample;
